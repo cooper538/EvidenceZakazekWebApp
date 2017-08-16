aff404b41d53a889311938797bdea85a75c8a860
Tomáš <Tom@TOMAS-PC>
2017-08-16 08:47:44 +0200

    Add project files.

---
+++ EvidenceZakazekWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EvidenceZakazekWebApp.Startup))]
+namespace EvidenceZakazekWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
