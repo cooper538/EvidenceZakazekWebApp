25ebd0c3eb818a19445c255040521fb75ad5a64c
vrabec1330 <tomasstar.enko@gmail.com>
2017-08-17 17:49:43 +0200

    Improve api products (/api/Products/forTable)

--- EvidenceZakazekWebApp/Controllers/Api/ProductsController.cs
+++ EvidenceZakazekWebApp/Controllers/Api/ProductsController.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Reflection;
+using System.Collections;
+using Newtonsoft.Json;
 
 namespace EvidenceZakazekWebApp.Controllers.Api
 {
@@ -28,6 +31,39 @@ namespace EvidenceZakazekWebApp.Controllers.Api
                 .ToList();
 
             return _mapper.Map<IEnumerable<ProductDto>>(products);
+        }
+
+        [Route("~/api/products/forTable")]
+        public IHttpActionResult GetProductsForTable()
+        {
+            var products = _context.Products
+                .Include(p => p.Supplier)
+                .ToList();
+
+            var productDtos = _mapper.Map<IEnumerable<ProductDto>>(products);
+
+            // from https://stackoverflow.com/a/10542548/6355668
+            // without serialize it (problem with escaping)
+            var tableData = GetObjectArray(productDtos);
+
+            var columnHeaders = new[] {
+                new { title = "Id" },
+                new { title = "Jméno" },
+                new { title = "Objednací číslo" },
+                new { title = "Typové označení" },
+                new { title = "Cena" },
+                new { title = "Dodavatel" }
+            };
+            
+            return Ok(new {
+                data = tableData,
+                columns = columnHeaders
+            }); 
+        }
+
+        public static IEnumerable<object> GetObjectArray<T>(IEnumerable<T> obj)
+        {
+            return obj.Select(o => o.GetType().GetProperties().Select(p => p.GetValue(o, null)));
         }
     }
 }
