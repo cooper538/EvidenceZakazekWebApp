7c3f9b6b5f04bcce0d827cfa844dd04bba2efbce
vrabec1330 <tomasstar.enko@gmail.com>
2017-09-04 12:25:55 +0200

    Implemented ProductCategory action Delete

--- EvidenceZakazekWebApp/Controllers/Api/ProductCategoriesController.cs
+++ EvidenceZakazekWebApp/Controllers/Api/ProductCategoriesController.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using EvidenceZakazekWebApp.Models;
+using System.Data.Entity;
+using System.Linq;
 using System.Web.Http;
 
 namespace EvidenceZakazekWebApp.Controllers.Api
@@ -14,6 +16,31 @@ namespace EvidenceZakazekWebApp.Controllers.Api
         {
             _context = new ApplicationDbContext();
             _mapper = MvcApplication.MapperConfiguration.CreateMapper();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Detele(int id)
+        {
+            var categoryForDelete = _context.ProductCategories
+                .Include(pc => pc.PropertyDefinitions.Select(pd => pd.PropertyValues))
+                .Include(pc => pc.Products)
+                .Single(p => p.Id == id);
+
+            if (categoryForDelete == null)
+                return NotFound();
+
+            foreach (var propertyDefinition in categoryForDelete.PropertyDefinitions)
+            {
+                _context.PropertyValues.RemoveRange(propertyDefinition.PropertyValues);
+            }
+
+            _context.PropertyDefinitions.RemoveRange(categoryForDelete.PropertyDefinitions);
+            _context.Products.RemoveRange(categoryForDelete.Products);
+
+            _context.ProductCategories.Remove(categoryForDelete);
+            _context.SaveChanges();
+
+            return Ok();
         }
     }
 }
