5afcec53e5dc9316f3eeeac79b370b900d0f6ffb
vrabec1330 <tomasstar.enko@gmail.com>
2017-10-10 12:51:28 +0200

    Dynamic NoItemsFound msg for CRUD table

---
+++ EvidenceZakazekWebApp/Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using static System.Char;
+
+namespace EvidenceZakazekWebApp.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string LowercaseFirstLetter(this string str)
+        {
+            return ToLowerInvariant(str[0]) + str.Substring(1);
+        }
+    }
+}
